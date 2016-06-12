b97a4867cb0544ea81d76aecce90d1bd8a1c0627
toboid <toboid@users.noreply.github.com>
2016-06-12 12:18:31 +0100

    Change api to avoid confusion with promie api

---
+++ test/all.tests.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const expect = require('chai').expect;
+const All = require('../lib');
+
+describe('all', function () {
+  it('completes when one operand is executed', () => {
+    let allFinished = false;
+
+    const all = All();
+    const p1 = all.operand();
+    all.finished(() => {
+      allFinished = true;
+    });
+
+    expect(allFinished).to.eql(false);
+    p1();
+    expect(allFinished).to.eql(true);
+  });
+
+  it('completes when three operands are executed', () => {
+    let allFinished = false;
+
+    const all = All();
+    const p1 = all.operand();
+    const p2 = all.operand();
+    const p3 = all.operand();
+    all.finished(() => {
+      allFinished = true;
+    });
+
+    expect(allFinished).to.eql(false);
+    p1();
+    expect(allFinished).to.eql(false);
+    p2();
+    expect(allFinished).to.eql(false);
+    p3();
+    expect(allFinished).to.eql(true);
+  });
+
+  it('completes when finished called between operands', () => {
+    let allFinished = false;
+
+    const all = All();
+    const p1 = all.operand();
+    const p2 = all.operand();
+
+    expect(allFinished).to.eql(false);
+    p1();
+    all.finished(() => {
+      allFinished = true;
+    });
+    expect(allFinished).to.eql(false);
+    p2();
+    expect(allFinished).to.eql(true);
+  });
+
+  it('calls finished only once', () => {
+    let alreadyCalled = false;
+
+    const all = All();
+    const p1 = all.operand();
+    all.finished(() => {
+      if (alreadyCalled) {
+        throw new Error('finished already called');
+      }
+      alreadyCalled = true;
+    });
+
+    p1();
+    p1();
+  });
+});
